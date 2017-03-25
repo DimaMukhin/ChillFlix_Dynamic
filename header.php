a31978ddc9d15fd927d94833472f07ce4120d3c0
DimaMukhin <dima9348@gmail.com>
2017-03-25 18:45:48 -0500

    header

---
+++ header.php
@@ -0,0 +1,24 @@
+<!-- Header: logo, search-bar, user information -->
+<header class="row">
+
+	<!-- Logo -->
+	<a href="main.html"><img src="res/ChillFlix_logo.png" class="col-xs-4 img-responsive"/></a>
+	
+	<!-- search bar -->
+	<form class="form-inline col-xs-4" style="margin-top: 5%">
+		<div class="form-group" id="search_bar">
+			<input type="text" class="form-control" placeholder="Search">
+			<button type="submit" class="btn btn-default"><i class="fa fa-search fa-md"></i>Search</button>
+		</div>
+	</form>
+	
+	<!-- Profile picture, notification -->
+	<div class="col-xs-4" align="center" style="padding-top: 1%" id="firstStudent">
+		<i class="fa fa-bell fa-2x" style="margin-top: 8%; padding-left: 50%;"></i>
+		<div class="pull-right" style="padding-right: 2%;">
+			<a href="#"><img src="res/profile.jpeg" class="img-circle" style="width: 100px; display: inline"/>
+			<h4>Stu Dent</h4></a>
+		</div>
+	</div>
+
+</header>
